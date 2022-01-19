f34968ae1e475e5dad7607a82b63e51a1f6fc346
darlavalderrama <valderramadi@g.cofc.edu>
2022-01-19 13:38:36 -0500

    completed hw#1!

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,46 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Darla Valderrama
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: I will be implementing and executing Python programs as I do arithmetic.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("Volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    total_shots = eval(input("Enter the player's total shots: "))
+    shots_made = eval(input("Enter how many shots the player made: "))
+    total = shots_made / total_shots * 100
+    print("Shooting Percentage: ", total, "%")
 
 
 def coffee():
-    pass
+    cafe = eval(input("How many pounds of coffee would you like?: "))
+    coffee_costs = ((10.50 + 0.86) * cafe) + 1.50
+    print("You're total is: ", coffee_costs)
 
 
 def kilometers_to_miles():
-    pass
+    john = eval(input("How many kilometers did you travel?: "))
+    miles = john / 1.61
+    print("That's ", miles, "miles!")
 
 
 if __name__ == '__main__':
