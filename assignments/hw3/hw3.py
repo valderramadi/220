46a29ed7e1d31f4105f396f3e34c8123ab958bc4
darlavalderrama <valderramadi@g.cofc.edu>
2022-02-04 17:44:38 -0500

    completed hw#3!

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,57 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Darla Valderrama
+hw3.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: I will develop simple Python programs that use for loops while computing arithmetic.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def average():
-    pass
+    ask_grades = eval(input("How many grades will you enter? "))
+    acc = 0
+    for i in range(1, ask_grades + 1):
+        hw_grade = eval(input("What was your grade for hw " + str(1)))
+        acc += hw_grade
+    average_grade = acc / ask_grades
+    print(average_grade)
 
 
 def tip_jar():
-    pass
+    acc = 0
+    for i in range(5):
+        tip = eval(input("How much do you want to donate?: "))
+        acc += tip
+    print(acc)
 
 
 def newton():
-    pass
+    num_square = eval(input("What number is 'x?: "))
+    improve = eval(input("How many times should we improve the approximation? "))
+    prox = num_square /2
+    for i in range(improve):
+        prox = ((prox + (num_square / prox))/2)
+    print(num_square, prox)
 
 
 def sequence():
-    pass
+    series = eval(input("What is the number of terms in a series?: "))
+    for i in range(1, series + 1):
+        seq = 1 + (i // 2 * 2)
+        print(seq, end=" ")
 
 
 def pi():
-    pass
+    new_series = eval(input("What is the number of terms in a series?: "))
+    acc = 1
+    for i in range(new_series):
+        num = 2 + ((i//2) * 2)
+        den = 1 + ((i+1)//2) * 2
+        fraction = num / den
+        acc = acc * fraction
+    print(acc * 2)
 
 
 if __name__ == '__main__':
