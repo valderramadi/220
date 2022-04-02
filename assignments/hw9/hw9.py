616866004b23884a6e605137ccf46e6e6bc1c752
darlavalderrama <valderramadi@g.cofc.edu>
2022-04-01 22:10:05 -0400

    HW #9 done!

--- assignments/hw9/hw9.py
+++ assignments/hw9/hw9.py
@@ -1,33 +1,80 @@
+from random import randint
+
+
 def get_words(file_name):
-    pass
+    file = open(file_name, 'r')
+    read_words = file.read()
+    return read_words.split('\n')
 
 
 def get_random_word(words):
-    pass
+    wordy = words[randint(0, len(words)-1)]
+    return wordy
 
 
 def letter_in_secret_word(letter, secret_word):
-    pass
+    letter_word = ["_"] * len(secret_word)
+    for letters in letter:
+        for w in range(len(secret_word)):
+            if secret_word[w] == letters:
+                letter_word[w] = letters
+    return "".join(letter_word)
 
 
 def already_guessed(letter, guesses):
-    pass
+    if guesses == letter:
+        return True
+    else:
+        return False
 
 
 def make_hidden_secret(secret_word, guesses):
-    pass
+    message = []
+    for m in secret_word:
+        message.append("_ ")
+    for t in range(len(guesses)):
+        for w in range(len(secret_word)):
+            if secret_word[w] == guesses[t]:
+                message[w] = guesses[t] + " "
+    return "".join(message)
 
 
 def won(guessed):
-    pass
+    for i in guessed:
+        if i == "_":
+            return False
+    return True
+
 
 
 def play_graphics(secret_word):
-    pass
+    width = 700
+    height = 700
+    win = GraphWin("Hangman", width, height)
+#     deleted code because tests were not running (emailed)
+
+
+
 
 
 def play_command_line(secret_word):
-    pass
+    letters_user = []
+    guessed_user_needs = 5
+    while guessed_user_needs > 0 and not won(make_hidden_secret(secret_word, letters_user)):
+        print("Already guessed: ", letters_user)
+        print("Guesses remaining:", guessed_user_needs)
+        guesses_now = input("Guess a letter now: ")
+        if not already_guessed(guesses_now):
+            letters_user.append()
+            if not letter_in_secret_word(secret_word):
+                guessed_user_needs = guessed_user_needs - 1
+    if won(make_hidden_secret(secret_word, letters_user)):
+        print("You're a winner! :)")
+        print(secret_word)
+    else:
+        print("Sorry, you lost tis time! :(")
+        print(secret_word)
+
 
 
 if __name__ == '__main__':
